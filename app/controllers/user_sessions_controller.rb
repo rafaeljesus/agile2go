a4414c3c534d9f04311fc7f4e2f93758548c404a
rafaeljesus <rafael_jesus86@hotmail.com>
2013-12-24 19:42:10 -0200

    create user_sessions_controller

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,18 @@
+class UserSessionsController < ApplicationController
+  respond_to :js
+
+  def new
+    @session = UserSession.new(session)
+  end
+
+  def create
+    @session = UserSession.new(session, params[:user_session])
+    flash[:notice] = t('flash.notice.signed_in') if @session.authenticate
+    respond_with @session, location: root_path
+  end
+
+  def destroy
+    flash[:notice] = t('flash.notice.signed_out') if user_session.destroy
+    respond_with user_session, location: root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Agile2go::Application.routes.draw do
+  get "user_sessions_controllers/new"
+  get "user_sessions_controllers/create"
+  get "user_sessions_controllers/destroy"
   resources :users
 
   get "home/index"
