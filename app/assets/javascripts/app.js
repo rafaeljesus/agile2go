20e0056eb8176521883e3022183de9e7d1f2a8c8
rafaeljesus <rafael_jesus86@hotmail.com>
2014-01-26 14:28:34 -0200

    fix rendering when reloading page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -19,18 +19,17 @@ var App = new (Backbone.View.extend({
   },
 
   init: function(){
-    var current_user = new App.Models.CurrentUser({});
-    if (!current_user.signed_in()){
-      current_user.fetch({});
-      new App.Views.Menu({ current_user: current_user });
-      new App.Routers.Site({ current_user: current_user });
-      new App.Routers.UserRegistrations({ current_user: current_user });
-      new App.Routers.UserSessions({ current_user: current_user });
-      new App.Routers.Projects({});
-      new App.start();
-      new App.semanticUI();
-    }
- }
+    var session = JSON.parse(sessionStorage.getItem('currentUser'));
+    var current_user = new App.Models.CurrentUser(session);
+    if (!current_user.get('signed_in')) { current_user.fetch({}); }
+    new App.Views.Menu({ current_user: current_user });
+    new App.Routers.Site({ current_user: current_user });
+    new App.Routers.UserRegistrations({ current_user: current_user });
+    new App.Routers.UserSessions({ current_user: current_user });
+    new App.Routers.Projects({});
+    new App.start();
+    new App.semanticUI();
+  }
 
 }))({ el: document.body });
 
