8743e78cf21c0a9695ca991ab0b07a46600fa250
rafaeljesus <rafael_jesus86@hotmail.com>
2014-01-28 23:50:08 -0200

    add comments on router_helper mixin

--- app/assets/javascripts/mixins/router_helper.js
+++ app/assets/javascripts/mixins/router_helper.js
@@ -1,5 +1,6 @@
 App.Mixins.RouterHelper = {
 
+  // TODO listen for router changes instead of calling this.authorize in every router method
   authorize: function(){
     if (!this.current_user || !this.current_user.get('signed_in')) {
       window.location.hash = '/sessions/new';
