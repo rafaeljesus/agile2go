37072dfe42840b6d92c79b75aeface983760139a
rafaeljesus <rafael_jesus86@hotmail.com>
2014-01-30 23:54:34 -0200

    refactory hash on router

--- app/assets/javascripts/views/user_sessions/user_sessions_destroy.js
+++ app/assets/javascripts/views/user_sessions/user_sessions_destroy.js
@@ -10,7 +10,7 @@ App.Views.UserSessionsDestroy = Support.CompositeView.extend({
       self.current_user.set({ signed_in: false });
       self.current_user.removeSession({});
       self.logoutSuccess();
-      window.location.hash = '/';
+      window.location.hash = '#';
     }).fail(function(error){
       console.log(error);
     });
