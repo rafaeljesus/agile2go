7bd7e45976181d2238f9ea638e608be31e7b1b0c
rafaeljesus <rafael_jesus86@hotmail.com>
2014-02-08 13:49:59 -0200

    covering and refactory jasmine

--- spec/javascripts/views/projects_index_spec.js
+++ spec/javascripts/views/projects_index_spec.js
@@ -1,17 +1,25 @@
 describe('App.Views.ProjectsIndex', function(){
 
-  var collection = new App.Collections.Projects({});
+  var $el
+  , view
+  , collection = new App.Collections.Projects({});
 
   beforeEach(function(){
     var attributes = { id: 1, name: 'nameFake', company: 'companyFake', description: 'descriptionFake', assignedUsers: [{ id: 1, name: 'userNameFake' }] };
     collection.reset(attributes);
+    view = new App.Views.ProjectsIndex({ collection: collection });
+    $el = $(view.render().el);
   });
 
   it('should renders a collections of projects', function(){
-    var view = new App.Views.ProjectsIndex({ collection: collection });
-    var $el = $(view.render().el);
     expect($el).toHaveText(/nameFake/);
     expect($el).toHaveText(/companyFake/);
     expect($el).toHaveText(/descriptionFake/);
+  });
+
+  it('should display confirm modal', function(){
+    spyOn(view, 'showModal');
+    view.showModal();
+    expect(view.showModal).toHaveBeenCalled();
   });
 });
