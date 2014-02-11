cbfd32d4343d720b51c987f4553d5b9431fde059
rafaeljesus <rafael_jesus86@hotmail.com>
2014-02-10 23:46:44 -0200

    cover sprint_spec rspec

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,11 +1,10 @@
 class Sprint < ActiveRecord::Base
-  DATE_REGEX = /$\d{4}-\d{2}-\d{2}/
-  POINTS = [ 1, 2, 3, 5, 8, 13, 20, 40, 100]
+  POINTS = [ 1, 2, 3, 5, 8, 13, 20, 40, 100 ]
 
   belongs_to :project
 
-  validates :points, numericality: true, inclusion: { in: POINTS }
-  validates :start_date, presence: true , format: DATE_REGEX
-  validates :end_date, presence: true, format: DATE_REGEX
+  validates :points, presence: true, numericality: { only_integer: true }, inclusion: { in: POINTS }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates :project_id, presence: true
 end
--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -6,11 +6,9 @@ describe Sprint do
   it { should validate_presence_of(:project_id) }
   it { should validate_presence_of(:points) }
   it { should ensure_inclusion_of(:points).in_array(Sprint::POINTS) }
-  it { validates_numericality_of :points, only_integer: true }
+  it { should validate_numericality_of(:points) }
   it { should allow_value(Date.today).for(:start_date) }
   it { should allow_value(Date.today).for(:end_date) }
   it { should allow_value(Time.now).for(:daily) }
-  it { should allow_value('2014-01-01').for(:start_date) }
-  it { should allow_value('2014-01-01').for(:end_date) }
-
+  it { should belong_to :project }
 end
