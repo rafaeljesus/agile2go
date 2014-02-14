0b7ba6cd8cdd6cd083c4e57c1aabd71bc20c889c
rafaeljesus <rafael_jesus86@hotmail.com>
2014-02-13 23:24:04 -0200

    configuring puma to work on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,5 @@ Agile2go::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
-
-  # config.assets.initialize_on_precompile = true
+  config.assets.debug = true
 end
