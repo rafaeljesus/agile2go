16e35122b395067e076c65273bbb27a80a32df97
rafaeljesus <rafael_jesus86@hotmail.com>
2014-02-16 22:20:47 -0300

    deploy heroku

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -21,6 +21,7 @@ var App = new (Backbone.View.extend({
 
   init: function(){
     new App.setI18nHbsHelper();
+
     var current_user = new App.Models.CurrentUser({});
     var injector = { current_user: current_user };
     new App.Views.Menu(injector);
