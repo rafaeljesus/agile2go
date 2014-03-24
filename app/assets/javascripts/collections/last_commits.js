9a898ce58c474764941c152680a9f67fdf3ee450
Rafael Jesus <rafael_jesus86@hotmail.com>
2014-03-24 11:10:02 -0300

    Update last_commits.js

--- app/assets/javascripts/collections/last_commits.js
+++ app/assets/javascripts/collections/last_commits.js
@@ -1,5 +1,5 @@
 App.Collections.LastCommits = Backbone.Collection.extend({
-  url: 'https://api.github.com/repos/oncrafting/agile2go/commits',
+  url: 'https://api.github.com/repos/rafaeljesus/agile2go/commits',
   model: App.Models.LastCommit,
 
   parse: function(response){
