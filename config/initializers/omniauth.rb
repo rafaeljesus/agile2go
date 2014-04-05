068354e75a20c80c12b5087be6104222ec0c77ba
rafaeljesus <rafael_jesus86@hotmail.com>
2014-04-05 02:21:43 -0300

    uncommented omniauth.rb twitter provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :developer unless Rails.env.production?
-  # provider :twitter, '1kGu0lZIsid84fFA6RwUzA', 'dapabZg4VXYSGlVDOWVuYFuqK82BMV511w7XPBmMLo',
+  provider :twitter, '1kGu0lZIsid84fFA6RwUzA', 'dapabZg4VXYSGlVDOWVuYFuqK82BMV511w7XPBmMLo'
   provider :facebook, '621737011247997', '072157a2fcb5f2d483106b91f11383ff'
 end
 
