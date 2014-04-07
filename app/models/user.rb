ed9b8a0d9dd0a4ac8045d032ba4a3f32ae2d5547
rafaeljesus <rafael_jesus86@hotmail.com>
2014-04-07 19:51:17 -0300

    fix new user unable to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   rolify
 
+  attr_reader :password
+
   has_many :assignments
   has_many :projects, through: :assignments
   has_many :assigned_projects, through: :assignments, class_name: "Project", source: :project
@@ -9,7 +11,7 @@ class User < ActiveRecord::Base
     minimum: 4, too_short: "%{count} is the minimum allowed",
     maximum: 100, too_large: "%{count} is the maximum allowed" }
 
-  has_secure_password validations: false
+  has_secure_password if: -> (m) { m.password.present? }
   after_save :clear_password
 
   def self.authenticate(email, password)
@@ -17,7 +19,7 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    auth['uid'] = auth['uid'].to_s # workaround for github bug
+    auth['uid'] = auth['uid'].to_s
     where(auth.slice(:provider, :uid)).first || create_from_omniauth(auth)
   end
 
