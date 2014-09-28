d94759d2fc16e534f05dba425c6eb1dd9c393640
Rafael Jesus <rafael_jesus86@hotmail.com>
2014-09-28 00:23:47 -0300

    increase capybara timeout for 30

--- spec/support/features/task_helpers.rb
+++ spec/support/features/task_helpers.rb
@@ -8,14 +8,12 @@ module Features
 
     def update_task task
       visit "#tasks/#{task.id}/edit"
-      sleep 1
       fill_in 'title', with: 'new title'
       submit
     end
 
     def delete_task task
       visit '#tasks'
-      sleep 1
       find(".confirm").click
       find(".delete").click
     end
@@ -23,18 +21,18 @@ module Features
     private
     def create_task_as new_task
       visit '#tasks/new'
-      sleep 1
-      page.execute_script("$('#status')[0].value = '#{new_task.status}'")
-      page.execute_script("$('#priority')[0].value = '#{new_task.priority}'")
-      page.execute_script("$('#points')[0].value = '#{new_task.points}'")
-      page.fill_in 'title', with: new_task.title
-      page.fill_in 'story', with: new_task.story
-      page.execute_script("$('select').val(#{new_task.sprint.id}).trigger('change');")
+      sleep 0.5
+      execute_script("$('#status')[0].value = '#{new_task.status}'")
+      execute_script("$('#priority')[0].value = '#{new_task.priority}'")
+      execute_script("$('#points')[0].value = '#{new_task.points}'")
+      fill_in 'title', with: new_task.title
+      fill_in 'story', with: new_task.story
+      execute_script("$('select').val(#{new_task.sprint.id}).trigger('change');")
       submit
     end
 
     def submit
-      page.find("[type='submit']").click
+      find("[type='submit']").click
     end
 
   end
