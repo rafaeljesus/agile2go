3d5b907ac9c9c1d60939a6b0326efc5f9736d560
Rafael Jesus <rafael_jesus86@hotmail.com>
2014-10-21 00:22:16 -0200

    cover basic behavior

--- app/services/event.rb
+++ app/services/event.rb
@@ -1,9 +1,8 @@
-require 'net/http'
-
 class Event
+
   def initialize(model, event)
     base_uri = 'scorching-heat-1523.firebaseio.com'
-    @ferebase = Firebase::Client.new(base_uri)
+    @firebase = Firebase::Client.new(base_uri)
     @model = model
     @event = event
   end
---
+++ spec/services/sync_dashboard_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe SyncDashboard do
+
+  before do
+    project = FactoryGirl.create(:project)
+    @dashboard = Dashboard.create(project_name: project.name)
+    sprint = FactoryGirl.create(:sprint, project_id: project.id)
+    @task = FactoryGirl.create(:task, sprint_id: sprint.id)
+  end
+
+  it 'when create event then increase status in dashboard' do
+    sync = SyncDashboard.new(@task, :create)
+    sync.update_dashboard
+    dashboard = Dashboard.find(@dashboard.id)
+    expect(dashboard.todo_count).to eq(1)
+  end
+
+  it 'when update event then increase status in dashboard' do
+    sync = SyncDashboard.new(@task, :create)
+    sync.update_dashboard
+    dashboard = Dashboard.find(@dashboard.id)
+    expect(dashboard.todo_count).to eq(1)
+  end
+
+  it 'when destroy event then increase status in dashboard' do
+    sync = SyncDashboard.new(@task, :destroy)
+    sync.update_dashboard
+    dashboard = Dashboard.find(@dashboard.id)
+    expect(dashboard.todo_count).to eq(0)
+  end
+end
